96ab8fe16c3de6b97bbca49938a37a3ae1ad1cad
zdoh <denfairycat@gmail.com>
2019-03-15 13:31:43 +0300

    Initial commit

---
+++ src/main/java/ru/zdoher/otushomework3/OtusHomework3Application.java
@@ -0,0 +1,13 @@
+package ru.zdoher.otushomework3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OtusHomework3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OtusHomework3Application.class, args);
+    }
+
+}
